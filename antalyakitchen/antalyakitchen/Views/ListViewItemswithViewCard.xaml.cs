ace05f020e3e6b31a1baaa3d0710e9ebe3c898dd
Ali Safder <alisafder@gmail.com>
2021-03-31 13:57:51 +0500

    Working with ListView

--- antalyakitchen/antalyakitchen/Views/ListViewItemswithViewCard.xaml.cs
+++ antalyakitchen/antalyakitchen/Views/ListViewItemswithViewCard.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Android.Support.V7.App;
+using System;
 using System.Collections.Generic;
+
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,21 +23,93 @@ namespace antalyakitchen.Views
         public ListViewItemswithViewCard()
         {
             InitializeComponent();
-            ListDetails = new ObservableCollection<CardView>
-            {
-                new CardView{ImgIcon="logo.png", Name="Bar BQ"},
-                new CardView{ImgIcon="logo.png", Name="Pizza"},
-                new CardView{ImgIcon="logo.png", Name="Burgers"}
-            };
+            ListDetails = GetTables();
+            //ListDetails = new ObservableCollection<CardView>
+            //{
+            //    new CardView{ItemImage="logo.png", itemName_En="Bar BQ"},
+            //    new CardView{ItemImage="as.png", itemName_En="Pizza"},
+            //    new CardView{ItemImage="at.png", itemName_En="Burgers"}
+            //};
             BindingContext = this;
         }
 
+        public ObservableCollection<CardView> GetTables()
+        {
+            ObservableCollection<CardView> result = new ObservableCollection<CardView>();
+            DataTable dtItems = new DataTable();
+            string connectionString = "Data Source=192.168.10.4;Database=CertaNew;User Id=sa;Password=123";
+
+            string selectQuery = String.Format("select itemName_En,ItemImage from items where ItemImage is not null");
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //open connection
+                    //connection.Open();
+
+                    SqlCommand command = new SqlCommand(selectQuery, connection);
+                    connection.Open();
+
+                    command.Connection = connection;
+                    command.CommandText = selectQuery;
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    // this will query your database and return the result to your datatable
+                    da.Fill(dtItems);
+                    connection.Close();
+                    da.Dispose();
+
+                    //var cardview = new ObservableCollection<CardView>();
+                    foreach (DataRow row in dtItems.Rows)
+                    {
+                        result.Add(new CardView()
+                        {
+                            //ItemImage = row["ItemImage"],
+                            ItemImage = byteArrayToImage((byte[])row["ItemImage"]),
+                            itemName_En = (string)row["itemName_En"],
+                        });
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.Forms.MessageBox.Show(ex.Message.ToString());
+                //ex.Message.ToString();
+                App.Current.MainPage.DisplayAlert("Exception", ex.Message.ToString(), "OK");
+                return result;
+                
+            }
+        }
+
+        public ImageSource byteArrayToImage(byte[] byteArrayIn)
+        {
+            Image image = new Image();
+            //byte[] bytes = Convert.FromBase64String()
+            //MemoryStream ms = new MemoryStream(byteArrayIn);
+            image.Source = ImageSource.FromStream(() => new MemoryStream(byteArrayIn));
+            return image.Source;
+        }
+
+        //public Image LoadImage(String base_64)
+        //{
+        //    byte[] bytes = Convert.FromBase64String(base_64);
+
+        //    Image image;
+        //    using (MemoryStream ms = new MemoryStream(bytes))
+        //    {
+        //        image = System.Drawing.FromStream(ms);
+        //    }
+
+        //    return image;
+        //}
     }
+
+    
 
     public class CardView
     {
-        public string ImgIcon { get; set; }
-        public string Name { get; set; }
+        public ImageSource ItemImage { get; set; }
+        public string itemName_En { get; set; }
 
     }
 }
